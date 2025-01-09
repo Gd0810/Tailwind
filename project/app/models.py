ece376213426ee744569f3c51194b54acbd4670c
Gd0810 <gd8102000@gmail.com>
2025-01-09 17:37:02 +0530

    thersday

--- project/app/models.py
+++ project/app/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    color = models.CharField(max_length=50)
+    color = models.CharField(max_length=50, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
+    
+    
 
 # Create your models here.
